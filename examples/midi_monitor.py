e29b9f6847315d02adc745e9f75df2892c102e96
Alexandru M Stan <alex@hypertriangle.com>
2015-05-24 23:42:17 -0700

    Used a Queue into midi_monitor example
    It's not good practice to print stuff in the process callback.
    
    Fixes #15

--- examples/midi_monitor.py
+++ examples/midi_monitor.py
@@ -4,21 +4,24 @@
 
 import jack
 import binascii
+from queue import Queue
 
 client = jack.Client("MIDI-Monitor")
 port = client.midi_inports.register("input")
+queue = Queue(0)
 
 
 @client.set_process_callback
 def process(frames):
+    last_frame_time = client.last_frame_time
     for offset, data in port.incoming_midi_events():
-        # TODO: use ringbuffer
-        print("{0}: 0x{1}".format(client.last_frame_time + offset,
-                                  binascii.hexlify(data).decode()))
+        queue.put_nowait((last_frame_time + offset, data))
     return jack.CALL_AGAIN
 
 with client:
     print("#" * 80)
-    print("press Return to quit")
+    print("press Ctrl+C to quit")
     print("#" * 80)
-    input()
+    while True:
+        event_time, data = queue.get()
+        print("{0}: 0x{1}".format(event_time, binascii.hexlify(data).decode()))
